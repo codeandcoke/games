a047477e22a38badea89f703f4fb099670b125e4
Santiago Faci <santi@codeandcoke.com>
2025-11-20 01:58:16 +0100

    MariaDB via docker-compose, relaciones y endpoints, logs

--- src/main/java/com/svalero/games/controller/ReviewController.java
+++ src/main/java/com/svalero/games/controller/ReviewController.java
@@ -1,6 +1,5 @@
 package com.svalero.games.controller;
 
-import com.svalero.games.domain.Game;
 import com.svalero.games.domain.Review;
 import com.svalero.games.domain.User;
 import com.svalero.games.dto.GameDto;
@@ -12,12 +11,19 @@ import com.svalero.games.exception.UserNotFoundException;
 import com.svalero.games.service.GameService;
 import com.svalero.games.service.ReviewService;
 import com.svalero.games.service.UserService;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ReviewController {
@@ -28,10 +34,20 @@ public class ReviewController {
     private GameService gameService;
     @Autowired
     private UserService userService;
+    private final Logger logger = LoggerFactory.getLogger(ReviewController.class);
+
 
     @GetMapping("/reviews")
     public ResponseEntity<List<Review>> getAll() {
+        logger.info("GET /reviews");
         List<Review> reviews = reviewService.findAll();
+        return new ResponseEntity<>(reviews, HttpStatus.OK);
+    }
+
+    @GetMapping("/games/{gameId}/reviews")
+    public ResponseEntity<List<Review>> getGameReviews(@PathVariable long gameId) throws GameNotFoundException {
+        GameDto gameDto = gameService.findById(gameId);
+        List<Review> reviews = reviewService.findByGame(gameDto);
         return new ResponseEntity<>(reviews, HttpStatus.OK);
     }
 
@@ -40,11 +56,11 @@ public class ReviewController {
         return null;
     }
 
-    @PostMapping("/reviews")
-    public ResponseEntity<Review> addReview(@RequestBody ReviewInDto reviewInDto)
+    @PostMapping("/games/{gameId}/reviews")
+    public ResponseEntity<Review> addReview(@Valid @RequestBody ReviewInDto reviewInDto, @PathVariable long gameId)
             throws GameNotFoundException, UserNotFoundException{
         // TODO Añadir validación
-        GameDto gameDto = gameService.findById(reviewInDto.getGameId());
+        GameDto gameDto = gameService.findById(gameId);
         User user = userService.findById(reviewInDto.getUserId());
         Review review = reviewService.add(reviewInDto, gameDto, user);
         // TODO Devolver un objeto ReviewOutDto
@@ -70,6 +86,26 @@ public class ReviewController {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleException(UserNotFoundException unfe) {
         ErrorResponse errorResponse = ErrorResponse.notFound("The user does not exist");
+        logger.error("The user does not exist", unfe);
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(GameNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleException(GameNotFoundException gnfe) {
+        ErrorResponse errorResponse = ErrorResponse.notFound("The game does not exist");
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleException(MethodArgumentNotValidException manve) {
+        Map<String, String> errors = new HashMap<>();
+        manve.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName, message);
+        });
+
+        ErrorResponse errorResponse = ErrorResponse.validationError(errors);
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
